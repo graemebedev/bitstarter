e29368ef286bbccb194088d9e10ff04a3f35d1cf
Graeme Bennett <graemebedev@gmail.com>
2013-07-07 14:27:08 +0000

    Tweak index.html buf

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
+var fs = require('fs');
 var express = require('express');
+
+var readIndexHTML = function()
+{
+  var data = fs.readFileSync('./index.html', null);
+  return data.toString('utf8');
+}
+
+//readIndexHTML();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(readIndexHTML());
 });
 
 var port = process.env.PORT || 5000;
